b9c010954d5f9b894134c1ecdc8d6ab612f1b9fd
MykytaLiubshin <enotich13@gmail.com>
2020-02-26 15:14:06 +0200

    Did it

---
+++ LabWork3/sources/scripts/footer.js
@@ -0,0 +1,21 @@
+foot_pics = [
+    ["https://github.com/MykytaLiubshin",`gh_logo.png`],
+    ["https://gitlab.com/enotich13",`gl_logo.png`],
+    ["https://www.instagram.com/herrgott_fox/",`ins_logo.jpg`],
+    ["https://www.pornhub.com/users/enotfreehugs",`PH_logo.jpg`],
+    ["https://t.me/the_doom_guy",`tg_logo.png`],
+]
+
+const foot_pic = (link, img) => `
+<a href="${link}">
+<img src="media/logos/${img}" height="30" width="30">
+</a>`
+
+const footer = (feet = foot_pics) => `
+<footer>
+        <div class="footer-copyright text-center py-3">© 2077 Copyright 
+        ${feet.map(foot => foot_pic(foot[0],foot[1])).join('')}
+        </div>
+    </footer>
+        `
+document.querySelector('#footer').innerHTML = footer()
--- LabWork3/sources/scripts/navbar.js
+++ LabWork3/sources/scripts/navbar.js
@@ -1,18 +1,22 @@
 const customElements = [
     ['table.html', 'Table'],
     ['audio.html', 'Audio'],
-    ['video.htm', 'Video'],
+    ['video.html', 'Video'],
 ]
 
-const listElement = (link, name) => `<li class="nav-item"><a class="nav-link" href="${link}">${name}</a></li>`
-const NavBar = (elements = customElements) =>
-    `<nav class="cnavbar navbar-expand-lg navbar navbar-dark bg-dark">
+const listElement = (link, name, active='Table') => name === active
+?`<li class="nav-item active"> <a class="nav-link" href="${link}">${name} <span class="sr-only">(current)</span></a></li>`
+:`<li class="nav-item"><a class="nav-link" href="${link}">${name}</a></li>`
+
+const NavBar = (cur = table,elements = customElements) =>
+{
+    return`<nav class="cnavbar navbar-expand-lg navbar navbar-dark bg-dark fixed">
     <ul class="navbar-nav mr-auto align">
-    <li class="nav-item active">
-      <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
-    </li>
-    ${elements.map(el => listElement(el[0],el[1])).filter(el => el !== ",")}
+    ${elements.map(el => listElement(el[0],el[1],cur)).filter(el => el !== ",")}
     </ul>
     </nav>`
-document.querySelector('#navbar').innerHTML = NavBar();
+}
 
+try{document.querySelector('#navbar-table').innerHTML = NavBar('Table');}catch(error) {}
+try{document.querySelector('#navbar-audio').innerHTML = NavBar('Audio');}catch(error) {}
+try{document.querySelector('#navbar-video').innerHTML = NavBar('Video');}catch(error) {}
