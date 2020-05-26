e51caffcdaaf5bb6a6b8fa7bcde6377fc89beae6
MykytaLiubshin <enotich13@gmail.com>
2020-05-26 19:52:34 +0300

    Lab7

--- LabWork7/source/scripts/navbar.js
+++ LabWork7/source/scripts/navbar.js
@@ -7,9 +7,9 @@ const get_navbar = ( active ) => `
 <nav id="navbar_rock" class="navbar navbar_rock ${ active==1 ? "leftie" : "rightie" }">
       <nav class="nav nav-pills flex-column">
         <a class="nav-link ${ active==1 ? "activ"  : "" }" href="#top">1</a>
-        <a class="nav-link ${ active==2 ? "activ"  : "" }" href="#about_us">2</a>
+        <a class="nav-link " href="#about_us">2</a>
         <a class="nav-link ${ active==3 ? "activ"  : "" }" href="#our_new_vid">3</a>
-        <a class="nav-link ${ active==4 ? "activ"  : "" }" href="#bottom">4</a>
+        <a class="nav-link " href="#bottom">4</a>
       </nav>
     </nav>
 ` 
@@ -20,3 +20,4 @@ const set_nav = (active, id) => {
 set_nav( 0, ids[0] )
 set_nav( 2, ids[1] )
 
+const show = () => console.log("Showin it")
