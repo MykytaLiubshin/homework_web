c73331c58bbbf78eb39c67079beae401ea2dd3bc
MykytaLiubshin <enotich13@gmail.com>
2020-05-27 11:03:13 +0300

    changed 7th a bit

--- LabWork9/source2/one-pager-classes/components/App/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import reactDom from "react-dom";
-import "regenerator-runtime/runtime";
-import "whatwg-fetch";
-import { App } from "./components/App";
-
-reactDom.render(<App />, document.getElementById("root"));
